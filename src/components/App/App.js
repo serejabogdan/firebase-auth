1b14acdbe2fb170b561212058f025c0727e23e5b
Sereja <serejabgdn@gmail.com>
2021-05-19 20:41:06 +0300

    added: signIn, signUp, signOut

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 import FormInput from '../../shared/FormInput';
 
+import {hasAuthStateChanged, signup, signin, signOut} from '../../utils/auth';
+
 function App() {
+	const [authState, setAuthState] = useState(null);
+	const [signupState, setSignupState] = useState({login: 'admin@gmail.com', password: 'adminadmin', confirmPassword: ''});
+	const [signinState, setSigninState] = useState({login: 'admin@gmail.com', password: 'adminadmin'});
+
+	useEffect(() => {
+		const unsubscribe = hasAuthStateChanged(setAuthState);
+		console.log(authState);
+		return unsubscribe;
+	}, [authState]);
+
+	const handleSignupSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			await signup(signupState.login, signupState.password);
+		} catch {
+			console.error('Cant signup');
+		}
+	};
+	const handleSigninSubmit = (e) => {
+		e.preventDefault();
+		signin(signinState.login, signinState.password);
+	};
 	return (
 		<div className='App'>
-			<h1>Registration</h1>
-			<form className='App__registration-form'>
-				<FormInput label='Login:' />
-				<FormInput label='Password:' />
-				<FormInput label='Confirm password:' />
-				<button type='submit'>Go!</button>
-			</form>
-			<h1>Login</h1>
-			<form className='App__registration-form'>
-				<FormInput label='Login:' />
-				<FormInput label='Password:' />
-				<button type='submit'>Get in</button>
-			</form>
+			{!authState ? (
+				<>
+					<h1>Registration</h1>
+					<form className='App__registration-form' onSubmit={handleSignupSubmit}>
+						<FormInput label='Login:' stateKey='login' state={signupState} setState={setSignupState} />
+						<FormInput label='Password:' stateKey='password' state={signupState} setState={setSignupState} />
+						<FormInput label='Confirm password:' stateKey='confirmPassword' state={signupState} setState={setSignupState} />
+						<button type='submit'>Go!</button>
+					</form>
+					<h1>Login</h1>
+					<form className='App__registration-form' onSubmit={handleSigninSubmit}>
+						<FormInput label='Login:' stateKey='login' state={signinState} setState={setSigninState} />
+						<FormInput label='Password:' stateKey='password' state={signinState} setState={setSigninState} />
+						<button type='submit'>Get in</button>
+					</form>
+				</>
+			) : (
+				<button onClick={signOut}>LogOut</button>
+			)}
 		</div>
 	);
 }
