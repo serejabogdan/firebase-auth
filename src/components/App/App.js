b3cf059517b575ea1f39925687f4f7965b0e4442
Sereja <serejabgdn@gmail.com>
2021-05-19 19:02:55 +0300

    make layout and css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
-	return <div className='App'>React</div>;
+	return (
+		<div className='App'>
+			<h1>Registration</h1>
+			<form className='App__registration-form'>
+				<label>
+					Login: <input type='text' />
+				</label>
+				<label>
+					Password: <input type='text' />
+				</label>
+				<label>
+					Confirm password: <input type='text' />
+				</label>
+				<button type='submit'>Go!</button>
+			</form>
+			<h1>Login</h1>
+			<form className='App__registration-form'>
+				<label>
+					Login: <input type='text' />
+				</label>
+				<label>
+					Password: <input type='text' />
+				</label>
+				<button type='submit'>Get in</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
