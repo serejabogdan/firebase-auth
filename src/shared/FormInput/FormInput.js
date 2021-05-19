21b468aa3dac61827af2bc2ee6d7c82b64d4d7d4
Sereja <serejabgdn@gmail.com>
2021-05-19 20:40:34 +0300

    updated: FormInput

--- src/shared/FormInput/FormInput.js
+++ src/shared/FormInput/FormInput.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './FormInput.css';
 
-function FormInput({label}) {
+function FormInput({label, state, setState, stateKey}) {
 	return (
 		<div>
 			<label>
-				{label} <input type='text' />
+				{label} <input type='text' value={state[stateKey]} onChange={(e) => setState((prevState) => ({...prevState, [stateKey]: e.target.value}))} />
 			</label>
 		</div>
 	);
