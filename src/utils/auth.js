09142e97fe3038c97f526b095f199e60b13898d0
Sereja <serejabgdn@gmail.com>
2021-05-19 20:40:11 +0300

    fixed: signIn; added: signOut

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,9 +5,13 @@ export function signup(email, password) {
 }
 
 export function signin(email, password) {
-	return auth.createUserWithEmailAndPassword(email, password);
+	return auth.signInWithEmailAndPassword(email, password);
 }
 
 export function hasAuthStateChanged(setState) {
 	return auth.onAuthStateChanged((user) => setState(user));
+}
+
+export function signOut() {
+	return auth.signOut();
 }
